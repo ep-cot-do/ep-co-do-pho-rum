e315a68432d9a7e224a47e572eed6fe26af6eb6f
LTP <lamphat279@gmail.com>
2025-03-19 16:43:28 +0700

    notification

--- Backend/src/main/java/com/fcoder/Fcoder/model/dto/request/GameRequest.java
+++ Backend/src/main/java/com/fcoder/Fcoder/model/dto/request/GameRequest.java
@@ -9,10 +9,16 @@ import java.util.List;
 
 @Data
 public class GameRequest {
+    @NotNull(message = "Author ID is required")
     private Long authorId;
+    @NotBlank(message = "Title is required")
     private String title;
+    @NotBlank(message = "Description is required")
     private String description;
+    @NotBlank(message = "Thumbnail is required")
     private String thumbnail;
+    @NotNull(message = "URL is required")
     private String url;
+    @NotNull(message = "Category is required")
     private List<String> category;
 }
