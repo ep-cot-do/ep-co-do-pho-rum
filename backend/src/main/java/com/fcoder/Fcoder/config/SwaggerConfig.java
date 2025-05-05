3f22dd4b8a31432efac64e946101bcc57140cae9
SvnFrs <thaidvq.work@gmail.com>
2025-05-05 23:58:22 +0700

    test workflows

--- backend/src/main/java/com/fcoder/Fcoder/config/SwaggerConfig.java
+++ backend/src/main/java/com/fcoder/Fcoder/config/SwaggerConfig.java
@@ -1,46 +1,66 @@
 package com.fcoder.Fcoder.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Value("${swagger-url}")
     private String swaggerUrl;
+
     @Value("${server.servlet.context-path}")
     private String contextPath;
+
     @Value("${server.port}")
     private String port;
+
     @Bean
     public OpenAPI openAPI() {
         List<Server> serverList = new ArrayList<>();
+
+        // Local server - remove the context path from here since it's already included in Swagger UI
         var localServer = new Server();
-        localServer.setUrl(String.format("http://localhost:%s%s", port, contextPath));
+        localServer.setUrl(String.format("http://localhost:%s", port));
+        localServer.setDescription("Local Development Server");
         serverList.add(localServer);
+
+        // Production server if available
         if (!"null".equals(swaggerUrl)) {
             var server = new Server();
-            server.setUrl(String.format("%s%s", swaggerUrl, contextPath));
+            // Do not append the context path here
+            server.setUrl(swaggerUrl.replace(contextPath, ""));
+            server.setDescription("Production API Server");
             serverList.add(server);
         }
-        Info info = new Info().title("Fcoder API").version("1.0")
-                .description("Fcoder API Documentation");
-        var openAPI =  new OpenAPI().info(info).components(new Components()
-                .addSecuritySchemes("accessCookie", new SecurityScheme()
-                        .type(SecurityScheme.Type.APIKEY)
-                        .in(SecurityScheme.In.COOKIE)
-                        .name("ACCESS_TOKEN")
-                ));
+
+        Info info = new Info()
+            .title("Fcoder API")
+            .version("1.0")
+            .description("Fcoder API Documentation");
+
+        var openAPI = new OpenAPI()
+            .info(info)
+            .components(
+                new Components()
+                    .addSecuritySchemes(
+                        "accessCookie",
+                        new SecurityScheme()
+                            .type(SecurityScheme.Type.APIKEY)
+                            .in(SecurityScheme.In.COOKIE)
+                            .name("ACCESS_TOKEN")
+                    )
+            );
+
         openAPI.servers(serverList);
         return openAPI;
-    };
+    }
 }
